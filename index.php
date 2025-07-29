d083fa206d5e2520668e4ddf008ab15feeeaabb0
Luis Limon <Luis.Limon.iml@gmail.com>
2025-07-29 03:53:35 -0700

    reestructura de proyecto

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ $members = $pdo->query("SELECT m.*, ms.name AS membership_name
       --gray: #6c757d;
       --bg-dark: #1a1a2e;
       --bg-card: #16213e;
-      --text-primary: #443c3cff;
+      --text-primary: #ece5e5ff;
       --text-secondary: #4d4040ff;
     }
 

ac949559caf356574db4f602ec196a6f84a2815f
Luis Limon <Luis.Limon.iml@gmail.com>
2025-07-29 02:49:54 -0700

    reestructura de proyecto

--- backend/config.php
+++ backend/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USERNAME','u179371012_231');
-define('DB_PASSWORD','Gym2025*2025');
-define('DB_NAME','u179371012_gimnasio');
+define('DB_USERNAME','');
+define('DB_PASSWORD','');
+define('DB_NAME','');
 
 try {
   $pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME.";charset=utf8", DB_USERNAME, DB_PASSWORD);
@@ -11,3 +11,13 @@ try {
   die("Error al conectar: ".$e->getMessage());
 }
 ?>
+
+
+<?php
+// config.php
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=u179371012_gimnasio;charset=utf8", "u179371012_231", "Gym2025*2025");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die(json_encode(['success' => false, 'message' => "DB Error: ".$e->getMessage()]));
+}

2b453a36278e1e223fb33c87129ff07000c41491
Luis Limon <Luis.Limon.iml@gmail.com>
2025-07-29 02:08:19 -0700

    reestructura de proyecto

--- backend/add_member.php
+++ backend/add_member.php
@@ -1,5 +1,12 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+header('Content-Type: application/json');
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // Configuración de la base de datos
 $db_host = 'localhost';
